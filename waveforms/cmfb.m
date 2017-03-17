5c0f1e255ae24d4480b052648d33eeddf65f576a
Luke Morrison <morriluk@oregonstate.edu>
2017-03-17 16:48:32 -0700

    final version + matlab stuff

---
+++ waveforms/cmfb.m
@@ -0,0 +1,64 @@
+
+close all
+clc
+clearvars
+
+fp1=fopen('cmfb_gain_fast.txt','r');
+fp2=fopen('cmfb_gain_nominal.txt','r');
+fp3=fopen('cmfb_gain_slow.txt','r');
+f1=fscanf(fp1,'%f\t%f+j%f',[3,61]);
+f2=fscanf(fp2,'%f\t%f+j%f',[3,61]);
+f3=fscanf(fp3,'%f\t%f+j%f',[3,61])
+%y=fft(x)
+%plot(y)
+
+
+for i = 1:61
+    y12(i) = angle(f1(2,i) + f1(3,i)*j) * 180 / pi;
+    y10(i) = sqrt( (f1(2,i))^2 + (f1(3,i))^2 );
+    x(i) = f1(1,i);
+end
+y11 = 10*log(y10)
+
+for i = 1:61
+    y22(i) = angle(f2(2,i) + f2(3,i)*j) * 180 / pi;
+    y20(i) = sqrt( (f2(2,i))^2 + (f2(3,i))^2 );
+    x(i) = f2(1,i);
+end
+y21 = 10*log(y20)
+
+for i = 1:61
+    y32(i) = angle(f3(2,i) + f3(3,i)*j) * 180 / pi;
+    y30(i) = sqrt( (f3(2,i))^2 + (f3(3,i))^2 );
+    x(i) = f3(1,i);
+end
+y31 = 10*log(y30)
+
+figure('color', 'w');
+subplot(2,1,1);
+semilogx(x,y11);
+hold on
+semilogx(x,y21);
+hold on
+semilogx(x,y31);
+hold off
+xlim([1000 500000000])
+title('Common Mode Feedback Gain for all cases');
+%xlabel('Frequency (Hz)') % x-axis label
+ylabel('CMFB Gain (dB)') % y-axis label
+legend('Fast', 'Nominal', 'Slow');
+grid on;
+
+
+subplot(2,1,2);
+semilogx(x,y12);
+hold on;
+semilogx(x,y22);
+hold on;
+semilogx(x,y32);
+xlim([1000 500000000])
+title('Common Mode Feedback Phase for all cases');
+xlabel('Frequency (Hz)') % x-axis label
+ylabel('CMFB Phase (deg)') % y-axis label
+legend('Fast', 'Nominal', 'Slow');
+grid on
---
+++ waveforms/swing.m
@@ -0,0 +1,40 @@
+% Don't forget to right click and add the HspiceToolbox directory to your path
+
+close all
+clc
+
+x_space = linspace(1.249,1.251,2001);
+x_space2 = linspace(1.249,1.251,2000);
+x_diff = diff(x_space)
+
+% Output curves
+x=loadsig('pro_final.sw0');
+
+V1= evalsig(x, 'v_out1');
+V2= -evalsig(x, 'v_out2');
+
+d1 = diff(V1);
+d2 = diff(-V2);
+
+
+figure('color','w');
+s(1) = subplot(2,1,1);
+plot(x_space2,d1/.000001);
+hold;
+plot(x_space2,d2/.000001);
+xlim([1.2497 1.2503]);
+ylabel('Differential Gain (V/V)') % y-axis label
+%set(s(1),'XTick',[]);
+grid on;
+title('Gain Curves in Fast case');
+
+s(2)=subplot(2,1,2);
+plot(x_space,V1);
+hold;
+plot(x_space,-V2);
+xlim([1.2497 1.2503])
+xlabel('Differential Input Voltage (V)') % x-axis label
+ylabel('Output Voltage (V)') % y-axis label
+title('Voltage Swing in Fast case')
+set(s,'box','off');
+grid on;
